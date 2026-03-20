ebe02c059222bad6077bdb82d9f646bf77569f5a
Celina <cg0640n@gre.ac.uk>
2026-03-20 14:17:52 +0000

    model

--- resnet.py
+++ resnet.py
@@ -2,14 +2,19 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-class Resnet(nn.Mdoule):
+class Resnet(nn.Module):
     def __init__(self):
         super (Resnet, self).__init__()
         self.backbone = models.resnet18(pretrained = True)
         num_features = self.backbone.fc.in_features
-        self.backbone.fc = nn.linear(num_features, 2)
+        self.backbone.fc = nn.Linear(num_features, 2)
 
     def forward(self, x):
         return self.backbone(x)
-    
-    
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model= Resnet().to(device) 
+
+criterion = nn.CrossEntropyLoss()
+optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
+
